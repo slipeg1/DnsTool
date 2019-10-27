4ca4d9c9e7d9268d134a0a2cfed2f3e395304675
slipeg <slipeg2@hotmail.fr>
2019-10-26 21:58:51 -0400

    Made the basic UI style,  no code yet.

--- DnsTool/Form1.Designer.cs
+++ DnsTool/Form1.Designer.cs
@@ -28,20 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.OutputWindow = new System.Windows.Forms.RichTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // OutputWindow
+            // 
+            this.OutputWindow.BackColor = System.Drawing.SystemColors.InfoText;
+            this.OutputWindow.ForeColor = System.Drawing.SystemColors.Info;
+            this.OutputWindow.Location = new System.Drawing.Point(120, 7);
+            this.OutputWindow.Name = "OutputWindow";
+            this.OutputWindow.ReadOnly = true;
+            this.OutputWindow.Size = new System.Drawing.Size(318, 294);
+            this.OutputWindow.TabIndex = 1;
+            this.OutputWindow.Text = "OUTPUTWINDOW";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkRed;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.OutputWindow);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 11);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(451, 315);
+            this.panel1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Firebrick;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "DNS Lookup";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // DnsToolWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(476, 338);
+            this.Controls.Add(this.panel1);
             this.Name = "DnsToolWindow";
             this.Text = "DnsTool";
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox OutputWindow;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DnsTool/Form1.cs
+++ DnsTool/Form1.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DnsClient;
 
 namespace DnsTool
 {
     public partial class DnsToolWindow : Form
     {
+        private LookupClient Client;
         public DnsToolWindow()
         {
             InitializeComponent();
+            Client = new LookupClient();
+            Client.UseCache = true;         
         }
     }
 }
