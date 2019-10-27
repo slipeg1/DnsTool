fa53851102810af2295e1398599b28e20d092195
slipeg <slipeg2@hotmail.fr>
2019-10-27 00:51:14 -0400

    Added a TextBox control for url input.

--- DnsTool/Form1.Designer.cs
+++ DnsTool/Form1.Designer.cs
@@ -30,7 +30,9 @@
         {
             this.OutputWindow = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.LookUpButton = new System.Windows.Forms.Button();
+            this.UrlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,10 +40,10 @@
             // 
             this.OutputWindow.BackColor = System.Drawing.SystemColors.InfoText;
             this.OutputWindow.ForeColor = System.Drawing.SystemColors.Info;
-            this.OutputWindow.Location = new System.Drawing.Point(120, 7);
+            this.OutputWindow.Location = new System.Drawing.Point(120, 53);
             this.OutputWindow.Name = "OutputWindow";
             this.OutputWindow.ReadOnly = true;
-            this.OutputWindow.Size = new System.Drawing.Size(318, 294);
+            this.OutputWindow.Size = new System.Drawing.Size(318, 274);
             this.OutputWindow.TabIndex = 1;
             this.OutputWindow.Text = "OUTPUTWINDOW";
             // 
@@ -49,38 +51,64 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.DarkRed;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.UrlTextBox);
+            this.panel1.Controls.Add(this.LookUpButton);
             this.panel1.Controls.Add(this.OutputWindow);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(12, 11);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(451, 315);
+            this.panel1.Size = new System.Drawing.Size(451, 341);
             this.panel1.TabIndex = 2;
             // 
-            // button1
+            // LookUpButton
             // 
-            this.button1.BackColor = System.Drawing.Color.Red;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Firebrick;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(3, 7);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 28);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "DNS Lookup";
-            this.button1.UseVisualStyleBackColor = false;
+            this.LookUpButton.BackColor = System.Drawing.Color.Red;
+            this.LookUpButton.FlatAppearance.BorderColor = System.Drawing.Color.Firebrick;
+            this.LookUpButton.FlatAppearance.BorderSize = 2;
+            this.LookUpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LookUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LookUpButton.Location = new System.Drawing.Point(3, 52);
+            this.LookUpButton.Name = "LookUpButton";
+            this.LookUpButton.Size = new System.Drawing.Size(111, 28);
+            this.LookUpButton.TabIndex = 2;
+            this.LookUpButton.Text = "DNS Lookup";
+            this.LookUpButton.UseVisualStyleBackColor = false;
+            // 
+            // UrlTextBox
+            // 
+            this.UrlTextBox.BackColor = System.Drawing.Color.Black;
+            this.UrlTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.UrlTextBox.ForeColor = System.Drawing.SystemColors.Info;
+            this.UrlTextBox.Location = new System.Drawing.Point(137, 17);
+            this.UrlTextBox.Name = "UrlTextBox";
+            this.UrlTextBox.Size = new System.Drawing.Size(301, 26);
+            this.UrlTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Brown;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(3, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "URL to resolve :";
             // 
             // DnsToolWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(476, 338);
+            this.ClientSize = new System.Drawing.Size(476, 364);
             this.Controls.Add(this.panel1);
             this.Name = "DnsToolWindow";
             this.Text = "DnsTool";
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -89,7 +117,9 @@
 
         private System.Windows.Forms.RichTextBox OutputWindow;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button LookUpButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UrlTextBox;
     }
 }
 
